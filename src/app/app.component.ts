b7ed223bff63293f7e07491dd0fdf73a6fb55536
imskrishnan <imskrishnan@gmail.com>
2023-07-13 23:38:52 -0400

    handle change events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password: string = '';
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
 
-  onButtonClick() {
-    this.password = 'Password123';
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
   }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {}
 }
